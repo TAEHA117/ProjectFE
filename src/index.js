2709ababcad6d1e09888a32f3eab1ffb98345d0f
TAEHA117 <nn_goods@naver.com>
2023-11-13 11:59:18 +0900

    중간 세팅

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import ErrorPage from './pages/commons/ErrorPage';
 import reportWebVitals from './reportWebVitals';
 
 import './i18n';
@@ -11,12 +13,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <App />
+      <BrowserRouter>
+        <ErrorPage>
+          <App />
+        </ErrorPage>
+      </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/pages/commons/NotFound.js
@@ -0,0 +1,6 @@
+// 이미지는 import해서 불러올 수 있음
+const NotFound = () => {
+  return <h1>페이지없음</h1>;
+};
+
+export default NotFound;
